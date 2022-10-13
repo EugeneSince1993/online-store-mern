4de65bcea5048be11fd6d8318c76c648fec34560
EugeneSince1993 <jk235@mail.ru>
2022-10-13 19:21:27 +0300

    ready to deploy again

--- client/src/redux/product/asyncActions.ts
+++ client/src/redux/product/asyncActions.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from '../../axios';
+import axios from 'axios';
 import { IProduct } from '../../types/IProduct';
 import { IObjWithAnyData, SearchProductParams } from './types';
 import pickBy from 'lodash/pickBy';
