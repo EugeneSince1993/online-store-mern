f9a53d4246c5a0cfd5a5316031fb77f281bb415f
EugeneSince1993 <jk235@mail.ru>
2022-12-15 18:18:35 +0300

    cart and favorites links fixed in header

--- server/dist/client/src/redux/auth/selectors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.selectIsAuth = exports.selectAuth = void 0;
-const selectAuth = (state) => state.auth;
-exports.selectAuth = selectAuth;
-const selectIsAuth = (state) => Boolean(state.auth.data);
-exports.selectIsAuth = selectIsAuth;
-//# sourceMappingURL=selectors.js.map
--- server/dist/client/src/redux/filter/selectors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.selectSort = exports.selectFilter = void 0;
-const selectFilter = (state) => state.filter;
-exports.selectFilter = selectFilter;
-const selectSort = (state) => state.filter.sort;
-exports.selectSort = selectSort;
-//# sourceMappingURL=selectors.js.map
